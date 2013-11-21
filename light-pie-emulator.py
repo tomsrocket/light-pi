8d2b576733136765c15d411ce92ef0f44c2a352b
Thomas Werner <twerner@mountainnews.com>
2013-11-21 22:01:45 +0100

    cleaned up the code a bit

--- light-pie-emulator.py
+++ light-pie-emulator.py
@@ -12,14 +12,14 @@ import pygame
 def logg( msg):
     print msg;
 
-
+SIZE = 32*5
 
 #
 # LedStrip
 # Singleton class to control the LED strip
 #
 class LedStrip:
-    size        = 32*5
+    size        = SIZE
     column      = bytearray( size * 3 + 1) # Whole strip in RGB 
     ledStrip    = bytearray( size * 3 + 1) # Whole strip in gamma corrected data, ready to be sent to spidev.
     gammaLookup = bytearray(256)
@@ -47,8 +47,42 @@ class LedStrip:
     def getSize(self):
         return self.size
 
-    def show(self ):
+
+    def sanitizeColor( self, c ):
+        sc = int(c)
+        if ( sc > 254):
+            sc = 254
+        if ( sc < 0):
+            sc = 0
+        return sc
+
+
+    def show( *args  ):
+        self = args[0]
+        leds = self.ledStrip
+        cols = self.column
+#        def lookupSum( a, b ): 
+#       TODO: calculate the sum of ledstrip and add
+
+        if ( len(args)>1 and args[1] != None ):
+            add = args[1]
+            cols = map(lambda x, y : x + y, self.column, add)
+
+        if ( len(args)>2 and args[2] != None ):
+            print "subbing"
+            sub = args[2]
+            cols = map(lambda x, y : x - y, self.column, sub)
+
+        for y in range(self.size):
+            y3=y*3
+            rr = self.sanitizeColor(cols[y3+0] )
+            rg = self.sanitizeColor(cols[y3+1] )
+            rb = self.sanitizeColor(cols[y3+2] )
+            pygame.draw.rect(self.screen,(rr,rg,rb),[y*6,20,5,5])
+        
         pygame.display.flip()
+
+
     #   spidev.write( self.ledStrip )
     #   spidev.flush()
 
@@ -58,21 +92,9 @@ class LedStrip:
 
     def setLedRgb(self, y, r,g,b ):
         y3 = y * 3
-        rr = int(r)
-        rg = int(g)
-        rb = int(b)
-        if ( rr > 254):
-            rr = 254
-        if ( rg > 254):
-            rg = 254
-        if ( rb > 254):
-            rb = 254
-        if ( rr <0 ):
-            rr=0
-        if ( rg < 0 ):
-            rg = 0
-        if ( rb < 0 ):
-            rb = 0
+        rr = self.sanitizeColor(r)
+        rg = self.sanitizeColor(g)
+        rb = self.sanitizeColor(b)
 
         self.ledStrip[y3 + 1] = self.gammaLookup[rr]
         self.ledStrip[y3]     = self.gammaLookup[rg]
@@ -81,7 +103,6 @@ class LedStrip:
         self.column[y3+1] = rg
         self.column[y3+2] = rb
 
-        pygame.draw.rect(self.screen,(rr,rg,rb),[y*6,20,5,5])
 
 
 
@@ -92,6 +113,49 @@ class LedStrip:
         rb = int(color[2])+self.column[y3+2]
         #logg( "set led color" + str(y) + ": (" + str(rr) + ", " + str(rg) + ", " + str(rb) )
         self.setLedRgb( y, rr,rg,rb )
+
+
+    def scrollLeft( self ):
+         for y in range( self.size-1 ):
+            y3 = y*3
+            self.setLedRgb( y, self.column[y3+3],self.column[y3+4],self.column[y3+5] )
+
+
+class TempColumn:
+
+    size        = SIZE
+    column      = bytearray( SIZE * 3 + 1) # Whole strip in RGB 
+
+    def __init__(self):
+        logg( "Initializing TempColumn" )
+        for i in range( SIZE ):
+            self.setLedRgb( i, 0,0,0)
+
+    def sanitizeColor( self, c ):
+        sc = int(c)
+        if ( sc > 254):
+            sc = 254
+        if ( sc < 0):
+            sc = 0
+        return sc
+
+    def scrollRight( self ):
+        for y in range( 1,self.size-1 ):
+            ym = self.size-y
+            y3 = ym*3
+            self.setLedRgb( ym, self.column[y3-3],self.column[y3-2],self.column[y3-1] )
+
+
+    def setLedRgb(self, y, r,g,b ):
+        y3 = y * 3
+        rr = self.sanitizeColor(r)
+        rg = self.sanitizeColor(g)
+        rb = self.sanitizeColor(b)
+
+        self.column[y3]   = rr
+        self.column[y3+1] = rg
+        self.column[y3+2] = rb
+
 
 
 
@@ -124,15 +188,91 @@ class LedEffekts:
         color  = ( color[0]*pc, color[1]*pc, color[2]*pc )
         return color
 
+    def initColor( self, ledStrip, color ):
+        for i in range(ledStrip.getSize() ):
+            ledStrip.setLedColor( i, color )
 
+
+
+class LedShow:
+
+    LedEffekts = None
+    LedStrip = None
+
+    def __init__(self):
+        logg( "Initializing LedShow" )
+        self.LedStrip = LedStrip()
+        self.LedEffekts = LedEffekts()
+
+    def stageLights( self ):
+        for count in range( 1000 ):
+            self.LedEffekts.initColor( self.LedStrip, (0,0,0) )
+            self.LedEffekts.cloud( self.LedStrip, 30,sinLed( count *0.02, 70 ), (255,0,0) )
+            self.LedEffekts.cloud( self.LedStrip, 40,sinLed( count *0.01, 70 ), (0,255,0) )
+            self.LedEffekts.cloud( self.LedStrip, 40,sinLed( count *0.015, 70 ), (0,0,255) )
+            self.LedStrip.show()
+            time.sleep(0.01)
+
+
+    def clear( self, type=0 ):
+        co = TempColumn()
+        mask = None
+        delay = 0.01
+        blocksize=1
+        if type > 0:
+            blocksize=5
+        for count in range(SIZE/blocksize):
+            if type > 0:
+                for i in range(blocksize):
+                    co.setLedRgb(1,255,255,255)
+                    co.scrollRight()
+                mask = co.column
+                delay = 0.05
+            if type < 1:
+                self.LedStrip.scrollLeft()
+            self.LedStrip.show( None, mask)
+            time.sleep( delay )
+
+
+    def colorChange( self ):
+        for count in range( 1000 ):
+            rr = sinLed(count*0.001, 127)
+            rg = sinLed(count*0.0001, 127)
+            rb = sinLed(count*0.01, 127)
+            self.LedStrip.setLedRgb(155, rr,rg,rb )
+            self.LedStrip.scrollLeft()
+            self.LedStrip.show()
+            time.sleep(0.001)
+
+    def yellowScroll( self ):
+
+        column2 = TempColumn()
+
+        for count in range( 1000 ):
+            rr = sinLed(count*0.01, 127)
+            rg = 0
+            rb = sinLed(count*0.005, 127)
+
+            self.LedStrip.setLedRgb(155, rr,rg,rb )
+            self.LedStrip.scrollLeft()
+
+            rr = rg = rb = 0
+            if (count % 160 < 24):
+                rr=rg=int( 127+math.sin(count*0.003)*127 )
+                rb=0
+
+            column2.setLedRgb( 1, rr, rg,rb )
+            if ( count%6  == 0 ):
+                column2.scrollRight()
+            summ = column2.column
+            self.LedStrip.show( summ )
+            time.sleep(0.01)
 
 
 
 def sinLed( count, width ):
     return int( width+math.sin( count )*width )
 
-def sinCake( count, width ):
-    logg( int( 127+math.sin( count )*127  ) )
 
 
 # how many LEDs do we have
@@ -144,53 +284,20 @@ column = bytearray(32*5 * 3 + 1)
 print "Displaying..."
 
 
-count =0
-rs = 255
-gs = 0
-bs = 0
-scroll = 0
 
-LedStrip = LedStrip()
-LedEffekts = LedEffekts();
+count =0
+ledShow = LedShow()
+
 
 while True:
-    rs=(rs-0.5)%255
-    bs=(bs+1)%255
-    scroll = scroll + 0.25
-    inp = count/100.0%(2*math.pi);
-
-    r = rs
-    b = bs
-    g = gs
-    for y in range(width):
-        b=b+1
-        r=r-1
+    print count
 
 
+    ledShow.colorChange()
+    ledShow.clear(1)
+    ledShow.stageLights()
+    ledShow.clear()
+    ledShow.yellowScroll()
+    ledShow.clear()
 
-
-
-
-        # save current LED color
-        rr=0
-        rg=0
-        rb=0
-        if (y + int(scroll)) % 30 == 0:
-            rr=rg=int( 127+math.sin(inp)*127 )
-            rb=0
-#
-#
-        LedStrip.setLedRgb(y,  rr,rg,rb )
-
-
-
-    LedEffekts.cloud( LedStrip, 30,sinLed( count *0.02, 70 ), (255,0,0) )
-    LedEffekts.cloud( LedStrip, 60,sinLed( count *0.01, 70 ), (0,255,0) )
-    LedEffekts.cloud( LedStrip, 40,sinLed( count *0.015, 70 ), (0,0,255) )
-
-
-    # show the colors on LED strip & screen
-    
     count=count+1
-    LedStrip.show()
-    time.sleep(0.01)
